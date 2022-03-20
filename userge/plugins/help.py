c63e2032b1c0ada8aeeae10cd3edb9cbd1454ab3
ASHWIN <66071679+notashwin@users.noreply.github.com>
2022-03-20 15:22:20 +0530

    Update help.py

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -178,7 +178,7 @@ if userge.has_bot:
             else:
                 user_dict = await userge.bot.get_user_dict(Config.OWNER_ID[0])
                 await c_q.answer(
-                    f"Only {user_dict['flname']} Can Access this...! Build Your USERGE-X",
+                    f"Chal hatt behen ki lwdi, chal hatt...",
                     show_alert=True,
                 )
 
